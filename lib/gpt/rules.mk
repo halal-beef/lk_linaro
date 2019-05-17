a70afec4815ca004f1babede905e692786702fbc
Kiwoong Kim <kwmad.kim@samsung.com>
2019-05-17 19:32:52 +0900

    GPT: create the brand new partition policy
    This function assumes host system submits the binary
    that contains three chunks including GPTs per LU when
    you use fastboot to configure partitions.
    And also assumes using a specific value for Attributes
    in GUID Partition Entry.
    
    All the GPTs would be stored in LU #0.
    
    Change-Id: Ie486922f5e6ba4616485170555e6b9b97aecad1b
    Signed-off-by: Kiwoong Kim <kwmad.kim@samsung.com>

--- lib/gpt/rules.mk
+++ lib/gpt/rules.mk
@@ -7,10 +7,16 @@ MODULE_DEPS += \
 
 GLOBAL_DEFINES += \
 	SYSPARAM_ALLOW_WRITE=1
-
+ifeq (1,$(INPUT_GPT_AS_PT))
+MODULE_SRCS += \
+	$(LOCAL_DIR)/gpt.c \
+	$(LOCAL_DIR)/guid.c
+else
 MODULE_SRCS += \
 	$(LOCAL_DIR)/pit.c \
 	$(LOCAL_DIR)/part_gpt.c \
 	$(LOCAL_DIR)/guid.c
+endif
+MODULE_SRCS += $(LOCAL_DIR)/part.c
 
 include make/module.mk
