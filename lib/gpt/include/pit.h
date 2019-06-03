30871b09965ef07edcac542ea6559e193f92530c
Kiwoong Kim <kwmad.kim@samsung.com>
2019-06-04 01:33:15 +0900

    PIT: refactor to make it compatible with part.c
    1. pit_init gets boot device id from part.c. I removed
    the code to decide boot device in pit. Instead, part.c does.
    
    2. part_write_partial at flash time calls pit_entry_write
    when using PIT. Both pit_entry_write and pit_access needs
    to handle sparse case, so I teared it away.
    
    3. And from now on, when you set filesys in pit binary to 6,
    you can do format partitions with F2FS.
    
    Change-Id: I9b7e0724d6031464c403c9f91cd2f8c9eba346db
    Signed-off-by: Kiwoong Kim <kwmad.kim@samsung.com>

--- lib/gpt/include/pit.h
+++ lib/gpt/include/pit.h
@@ -21,6 +21,61 @@
 #undef	PIT_DEBUG
 //#define PIT_DEBUG
 
+#define PIT_LBA_ALIGMENT		8		/* 4KB */
+#define PIT_SECTOR_SIZE			512
+#define PIT_MAGIC			0x12349876
+#define PIT_PART_META			47		/* based on GPT */
+
+#define PIT_DISK_LOC			(PIT_PART_META + 1)	/* for protective MBR */
+#define PIT_GPT_STRING_SIZE		0x2000
+
+#define PIT_EMMC_ERASE_SIZE		(1024)
+
+#define PIT_DISK_SIZE_LIMIT		16
+#define PIT_SIZE_LIMIT			(PIT_DISK_SIZE_LIMIT * PIT_SECTOR_SIZE)
+
+
+/*
+ * Setting this definition to smaller than maximum value is recommended.
+ */
+#define PIT_MAX_PART_NUM		59
+
+#define PIT_ENV_SIZE			(16 << 10)
+
+enum pit_filesys {
+	FS_TYPE_NONE	= 0,
+	FS_TYPE_BASIC,
+	FS_TYPE_SPARSE_EXT4	= 5,
+	FS_TYPE_SPARSE_F2FS,
+};
+
+/*
+ * PIT assumes that a unit size of block is 512 bytes.
+ * Therefore a sort of translation is necessary when passing it to
+ * block device drivers.
+ */
+struct pit_entry {
+	u32 reserved[4];
+	u32 filesys;		/* if this is gpt entry */
+	u32 blkstart;	/* overrided, start lba */
+	u32 blknum;	/* size as block */
+	u32 lun;	/* partition # */
+	u32 reserved1;
+	char name[32];		/* partition name */
+	u32 reserved2[8];
+	char option[32];	/* only for indication of 'remained' */
+} __attribute__((packed));
+
+struct pit_info {
+	u32 magic;	/* to check integrity */
+	u32 count;	/* a number of partitions */
+	u32 reserved[5];
+	struct pit_entry pte[PIT_MAX_PART_NUM];
+	u8 dummy[104];
+	u8 signature[272];
+} __attribute__((packed));
+
+
 enum pit_op {
 	PIT_OP_FLASH = 0,
 	PIT_OP_ERASE,
@@ -38,7 +93,8 @@ struct gpt_info {
 /*
  * Public functions
  */
-void pit_init(void);
+void pit_init(enum __boot_dev_id id);
+void pit_show_info(void);
 int pit_update(void *buf, unsigned int size);
 struct pit_entry *pit_get_part_info(const char *name);
 int pit_access(struct pit_entry *ptn, int op, u64 addr, u32 size);
