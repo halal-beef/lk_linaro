d0ebee10d29ad56e1cdb0ed8f69856ac9e5ae5f0
Taekgyun Ko <taeggyun.ko@samsung.com>
2019-07-10 12:17:25 +0000

    PROJECT: SMDK3830 base code
    Change-Id: I09f566347913e63cff388b1a350f432378932bc0
    Signed-off-by: Taekgyun Ko <taeggyun.ko@samsung.com>

---
+++ project/smdk3830.mk
@@ -0,0 +1,15 @@
+ARCH := arm64
+ARM_CPU := cortex-a53
+TARGET := smdk3830
+
+WITH_KERNEL_VM := 0
+WITH_LINKER_GC := 1
+
+MODULES += \
+	app/exynos_boot \
+	dev/acpm_ipc
+
+GLOBAL_DEFINES += \
+	INPUT_GPT_AS_PT=1 \
+	FIRST_GPT_VERIFY=0
+export INPUT_GPT_AS_PT=1
