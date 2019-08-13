e3f70a14f5baab8700c7a38658b3b6ba8e258756
Changki Kim <changki.kim@samsung.com>
2019-08-13 14:14:36 +0900

    Revert "PLATFORM: EXYNOS3830: disable DUMP EN"
    This reverts commit 23daec33e5efcc916a80ed24ca80da402e7d6850.
    
    Change-Id: I9ad8470a075750d03d1bc1b39d28f7da6b56dc93
    Signed-off-by: Changki Kim <changki.kim@samsung.com>

--- app/exynos_boot/exynos_boot.c
+++ app/exynos_boot/exynos_boot.c
@@ -244,7 +244,7 @@ reboot:
 	dfd_set_dump_en_for_cacheop(0);
 	set_debug_level("low");
 #else
-	dfd_set_dump_en_for_cacheop(0);
+	dfd_set_dump_en_for_cacheop(1);
 	set_debug_level("mid");
 #endif
 	set_debug_level_by_env();
