a010be3ab12ae581a4078a7af1670b6ff3311749
Sam Protsenko <semen.protsenko@linaro.org>
2020-09-02 13:59:03 +0300

    cmd_boot: Enable serial console in kernel
    Signed-off-by: Sam Protsenko <semen.protsenko@linaro.org>

--- platform/exynos3830/boot/cmd_boot.c
+++ platform/exynos3830/boot/cmd_boot.c
@@ -298,7 +298,7 @@ static int bootargs_process(void)
 	int ret = 0;
 
 	/* Below console value can be used for bootargs change */
-	/* update_val("console", "ttySAC0,115200n8"); */
+	update_val("console", "ttySAC0,115200n8");
 	update_val("androidboot.dtbo_idx", dtbo_idx);
 
 	/* reason */
