72b590f4feebbf9d178daf278ee02bff2fc1538b
Sam Protsenko <semen.protsenko@linaro.org>
2021-05-07 23:14:05 +0300

    [FIXES] Don't try to boot boot.img ver 3+
    Current bootloader is only capable of loading and running Android boot
    image version 2. In case when boot.img ver 3 is flashed, attempt to load
    it leads to data abort, thus making the board bricked (fastboot isn't
    functional in that case). In order to avoid that, check boot.img header
    version first, and if it's greater than 2, print corresponding error and
    fallback to fastboot mode gracefully, so that the user is able to
    re-flash correct images.
    
    There two functions have to be called before going to fastboot:
    
        sdm_encrypt_secdram();
        dfd_set_dump_en_for_cacheop(0);
    
    because otherwise the consequent reboot crashes with data abort in MMC
    driver.
    
    Signed-off-by: Sam Protsenko <semen.protsenko@linaro.org>

--- platform/exynos3830/boot/cmd_boot.c
+++ platform/exynos3830/boot/cmd_boot.c
@@ -650,6 +650,7 @@ int load_boot_images(void)
 	char boot_part_name[16] = "";
 	unsigned int ab_support = 0;
 	unsigned int boot_val = 0;
+	int err;
 
 	ab_support = ab_update_support();
 	boot_val = readl(EXYNOS3830_POWER_SYSIP_DAT0);
@@ -694,7 +695,9 @@ int load_boot_images(void)
 
 	part_read(part, (void *)BOOT_BASE);
 
-	cmd_scatter_load_boot(5, argv);
+	err = cmd_scatter_load_boot(5, argv);
+	if (err)
+		return err;
 #else
 	void *part;
 
@@ -721,6 +724,7 @@ int cmd_boot(int argc, const cmd_args *argv)
 	int gpio = 5;	/* Volume Up */
 #endif
 	unsigned int val;
+	int err;
 
 	fdt_dtb = (struct fdt_header *)DT_BASE;
 	dtbo_table = (struct dt_table_header *)DTBO_BASE;
@@ -757,7 +761,9 @@ int cmd_boot(int argc, const cmd_args *argv)
 	}
 #endif
 
-	load_boot_images();
+	err = load_boot_images();
+	if (err)
+		return err;
 
 	val = readl(EXYNOS3830_POWER_SYSIP_DAT0);
 	if (val == REBOOT_MODE_RECOVERY)
