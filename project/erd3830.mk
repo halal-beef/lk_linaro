a0ce86fd6f3ab9d88b7c165dea2d959127f94506
Jaehyoung Choi <jkkkkk.choi@samsung.com>
2022-09-14 16:01:28 +0900

    [3830] Add some drivers in erd3830
    Signed-off-by: Jaehyoung Choi <jkkkkk.choi@samsung.com>
    Change-Id: I039ff0d3f1513b0c84f124bf406efe2a4668c82c

--- project/erd3830.mk
+++ project/erd3830.mk
@@ -6,7 +6,7 @@ WITH_KERNEL_VM := 0
 WITH_LINKER_GC := 1
 
 MODULES += \
-	app/exynos_boot_3830 \
+	app/exynos_main \
 	dev/acpm_ipc \
 	dev/mmc
 
@@ -18,14 +18,22 @@ MODULE_DEPS += \
 	lib/libavb \
 	lib/lock \
 	lib/fastboot \
+	lib/fdtapi \
 	dev/rpmb \
 	external/lib/fdt \
 	external/lib/ufdt \
-	dev/adc
+	dev/adc \
+	dev/debug \
+	dev/power/pmic/s2mpu12 \
+	dev/power/pmic/s2mu106 \
+	dev/backlight_ic \
+	dev/i2c \
 
 GLOBAL_DEFINES += \
 	INPUT_GPT_AS_PT=1 \
 	FIRST_GPT_VERIFY=0 \
-	GPT_PART=1
+	GPT_PART=1 \
+	BOOT_IMG_HDR_V2=1
+
 export INPUT_GPT_AS_PT=1
 export GPT_PART=1
