e1b914946fe042f9801a241fdd92b1fa5325f4df
junbo.sim <junbo.sim@samsung.com>
2022-09-14 16:20:46 +0900

    dev: debug: add dss updated structure
    Change-Id: Ib0151bd6eee005623dbf69ebe70f42acf61a4775
    Signed-off-by: junbo.sim <junbo.sim@samsung.com>

--- dev/debug/rules.mk
+++ dev/debug/rules.mk
@@ -2,9 +2,16 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 
 MODULE := $(LOCAL_DIR)
 
+ifneq ($(CONFIG_EXYNOS_DSS_VERSION),)
+MODULE_SRCS += \
+	$(LOCAL_DIR)/dss_v$(CONFIG_EXYNOS_DSS_VERSION).c
+else
+MODULE_SRCS += \
+	$(LOCAL_DIR)/dss.c
+endif
+
 MODULE_SRCS += \
 	$(LOCAL_DIR)/dfd.c \
-	$(LOCAL_DIR)/dss.c \
 	$(LOCAL_DIR)/dss_store_ramdump.c
 
 include make/module.mk
