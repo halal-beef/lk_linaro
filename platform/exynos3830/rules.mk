9e1e6505f186e452d5e463c54549237d017893b7
Myungsu Cha <myung-su.cha@samsung.com>
2022-09-16 13:53:33 +0900

    [3830] platform: exynos3830: Enable itmon
    Signed-off-by: Myungsu Cha <myung-su.cha@samsung.com>
    Change-Id: I40e303ecd0f804d4d83c12e27f34368f769e6e8f

--- platform/exynos3830/rules.mk
+++ platform/exynos3830/rules.mk
@@ -27,6 +27,7 @@ MODULE_SRCS += \
 	$(LOCAL_DIR)/debug/dfd.c \
 	$(LOCAL_DIR)/debug/dss.c \
 	$(LOCAL_DIR)/debug/dfd_asm_helper.S \
+	$(LOCAL_DIR)/debug/itmon.c \
 	$(LOCAL_DIR)/board_rev/board_rev.c \
 	$(LOCAL_DIR)/dvfs_info.c \
 
