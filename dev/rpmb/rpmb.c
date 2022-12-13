halal-beef/lk_x1s

common: rpmb: change the name of rpmb smc call for consistency
Change-Id: Ie974c6b6efe685dcffb30a4b929e246bf9981511
Signed-off-by: Hyeyeon Chung <hyeon.chung@samsung.com>

dev/rpmb/rpmb.c
@@ -283,7 +283,7 @@ uint32_t set_RPMB_provision(uint64_t state)
 	uint64_t r3 = 0;
 	uint32_t ret = RV_SUCCESS;
 
-	r0 = SMC_AARCH64_PREFIX | SMC_SRPMB_PROVISIONED;
+	r0 = SMC_AARCH64_PREFIX | SMC_SRPMB_PROVISION;
 	r1 = state == 0?0:1;
 
 	ret = exynos_smc(r0, r1, r2, r3);

include/dev/rpmb.h
@@ -28,7 +28,7 @@ extern "C" {
 #define SMC_AARCH64_PREFIX			0xC2000000
 #define SMC_CM_RPMB				0x1018
 #define SMC_CM_DRBG				0x1019
-#define SMC_SRPMB_PROVISIONED			0x3818
+#define SMC_SRPMB_PROVISION			0x3818
 
 /* key and hmac length definition */
 #define RPMB_KEY_LEN				32

halal-beef/lk_linaro

Change-Id: I6db6499edb80eee1a0cc62fab18bba0545fc1941

dev/rpmb/rpmb.c
@@ -281,7 +281,7 @@ uint32_t set_RPMB_provision(uint64_t state)
 	uint64_t r3 = 0;
 	uint32_t ret = RV_SUCCESS;
 
-	r0 = SMC_AARCH64_PREFIX | SMC_SRPMB_PROVISIONED;
+	r0 = SMC_AARCH64_PREFIX | SMC_SRPMB_SET_PROVISION;
 	r1 = state == 0?0:1;
 
 	ret = exynos_smc(r0, r1, r2, r3);

include/dev/rpmb.h
@@ -30,7 +30,7 @@ extern "C" {
 //#define SMC_AARCH64_PREFIX			0xC2000000
 #define SMC_CM_RPMB				0x1018
 #define SMC_CM_DRBG				0x1019
-#define SMC_SRPMB_PROVISIONED			0x3818
+#define SMC_SRPMB_SET_PROVISION			0x3818
 
 /* key and hmac length definition */
 #define RPMB_KEY_LEN				32