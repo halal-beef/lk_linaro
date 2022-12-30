4a4fd1e2e99b7e7fe59c2d33429d58d93161490c
Myungsu Cha <myung-su.cha@samsung.com>
2022-12-30 17:59:32 +0900

    target: erd3830: support changing dss debug level
    Change-Id: I9411171aed417ce872d67499818d3e572ae8b9df
    Signed-off-by: Myungsu Cha <myung-su.cha@samsung.com>

---
+++ target/erd3830/include/target/dss_reconfig.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright@ Samsung Electronics Co. LTD
+ *
+ * This software is proprietary of Samsung Electronics.
+ *
+ * No part of this software, either material or conceptual may be copied or
+ * distributed, transmitted, transcribed, stored in a retrieval system or
+ * translated into any human or computer language in any form by any means,
+ * electronic, mechanical, manual or otherwise, or disclosed to third parties
+ * without the express written permission of Samsung Electronics.
+ *
+ */
+#ifndef __DSS_RECONFIG_H__
+#define __DSS_RECONFIG_H__
+
+void set_dss_debug_level_low(void);
+void set_dss_debug_level_high(void);
+
+#endif /* __DSS_RECONFIG_H__ */
